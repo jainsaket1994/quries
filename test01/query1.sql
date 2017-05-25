a63dc2193738c7b5ca818ee8b4f2644025dee827
saket <saket1194@gmail.com>
2017-05-25 13:29:05 +0530

    test01

---
+++ test01/query1.sql
@@ -0,0 +1,11 @@
+show databases;
+CREATE TABLE IF NOT EXISTS CUSTOMERS(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+   SALARY   DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+
+INSERT INTO CUSTOMERS VALUES ( 2 , 'SAKET01' , 3 , "SAGAR" , 22 );
